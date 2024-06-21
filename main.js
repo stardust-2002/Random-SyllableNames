dc0f7ea96f739a71aa1c4d037d10a15723f5aa5a
stardust-2002 <2984779414@qq.com>
2024-06-21 10:27:43 +0800

    数字底下添加唱名答案

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 function getRandomMusicalStaveNumber() {
     return Math.ceil(Math.random() * 7);
 }
+syllableNames = ["void", "do", "re", "mi", "fa", "so", "la", "si"]
 colNum = 7;
 lineNum = 10;
 contentDiv = document.querySelector("#syllableNames");
 for (let i = 0; i < lineNum; i++) {
-    str = "";
-    for (let j = 0; j < colNum; j++) {
-        str += "&nbsp;&nbsp;&nbsp;" + getRandomMusicalStaveNumber();
+    line = generateLineDiv(colNum);
+    console.log(line);
+    contentDiv.appendChild(line);
+}
+
+function generateLineDiv(colNum) {
+    result = document.createElement("div");
+    result.setAttribute("class", "line");
+    for (let i = 0; i < colNum; i++) {
+        oneSyllableNameDiv = document.createElement("div");
+        number = getRandomMusicalStaveNumber();
+        numberDiv = document.createElement("p");
+        syllableNameDiv = document.createElement("p");
+        syllableNameDiv.setAttribute("class", "answer");
+        numberDiv.textContent = number;
+        syllableNameDiv.textContent = syllableNames[number];
+        oneSyllableNameDiv.appendChild(numberDiv);
+        oneSyllableNameDiv.appendChild(syllableNameDiv);
+        result.appendChild(oneSyllableNameDiv);
     }
-    text = document.createElement("p");
-    text.innerHTML = str;
-    br = document.createElement("br");
-    contentDiv.appendChild(text);
-    contentDiv.appendChild(br);
+    return result;
 }
